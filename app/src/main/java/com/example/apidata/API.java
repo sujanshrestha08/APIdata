0bcc0c19ead869b948a3146413a6134b706de5d1
Silverspike <sujanstha406@gmail.com>
2019-05-13 01:34:29 +0545

    Added Update and Delete Employee from extracted API

--- app/src/main/java/com/example/apidata/API.java
+++ app/src/main/java/com/example/apidata/API.java
@@ -6,8 +6,10 @@ import model.Employee;
 import model.EmployeeCUD;
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.DELETE;
 import retrofit2.http.GET;
 import retrofit2.http.POST;
+import retrofit2.http.PUT;
 import retrofit2.http.Path;
 
 public interface API{
@@ -19,6 +21,12 @@ public interface API{
 
     @POST("create")
     Call<Void> registerEmployee(@Body EmployeeCUD emp);
+
+    @PUT("update/{empID}")
+    Call<Void> updateEmployee(@Path("empID") int empID, @Body EmployeeCUD emp);
+
+    @DELETE("delete/{empID}")
+    Call<Void> deleteEmployee(@Path("empID") int empId);
 
 }
 
--- app/src/main/java/com/example/apidata/RegisterEmployeeActivity.java
+++ app/src/main/java/com/example/apidata/RegisterEmployeeActivity.java
@@ -5,6 +5,15 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import model.Employee;
+import model.EmployeeCUD;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RegisterEmployeeActivity extends AppCompatActivity {
 
@@ -30,6 +39,31 @@ public class RegisterEmployeeActivity extends AppCompatActivity {
     }
     private void Register() {
         String name = etName.getText().toString();
-        Float salary = Float.parseFloat(etSalary.getText())
+        Float salary = Float.parseFloat(etSalary.getText().toString());
+        int age = Integer.parseInt(etEmpAge.getText().toString());
+
+        EmployeeCUD employee = new EmployeeCUD(name , salary, age);
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        API api = retrofit.create(API.class);
+
+        Call<Void> voidCall = api.registerEmployee(employee);
+
+        voidCall.enqueue(new Callback<Void>() {
+            @Override
+            public void onResponse(Call<Void> call, Response<Void> response) {
+                Toast.makeText(RegisterEmployeeActivity.this, "You have been successfully registered", Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onFailure(Call<Void> call, Throwable t) {
+                Toast.makeText(RegisterEmployeeActivity.this, "Error : " + t.getLocalizedMessage(), Toast.LENGTH_LONG).show();
+            }
+        });
+
     }
 }
